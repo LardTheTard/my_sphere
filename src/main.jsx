ddabc65f17e6a59752dcd04b51405049ac996dc2
Logan Zhao <l254zhao@uwaterloo.ca>
2026-08-06 04:52:17 -0400

    Add Game of Life favicon and mobile scale

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,12 @@ import App from './App.jsx'
 import MinimalPortfolio from './MinimalPortfolio.jsx'
 import './styles.css'
 
+if (location.pathname !== '/current') {
+  const favicon = document.getElementById('site-favicon')
+  favicon.href = '/life-favicon.png'
+  favicon.type = 'image/png'
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {location.pathname === '/current' ? <App /> : <MinimalPortfolio />}
